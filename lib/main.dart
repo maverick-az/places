c7b80052d7b197fc1ef661e405d02d29273ef352
Andrei <zariciuc.andrei@gmail.com>
2022-03-18 15:00:38 +0200

    Creadet MyFirstWidget (StatelessWidget)

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,22 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyFirstWidget(),
+    );
+  }
+}
+
+class MyFirstWidget extends StatelessWidget {
+  var _counter = 0;
+  MyFirstWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(++_counter);
+    return Container(
+      child: const Center(
+        child: Text('Hello!'),
+      ),
     );
   }
 }
