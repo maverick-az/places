314bc1ea810e95f7b28ae2d3b7d2ed8d04485450
Andrei <zariciuc.andrei@gmail.com>
2022-04-19 18:04:17 +0300

    Experimenting with Scaffold

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,7 +11,71 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('Список интересных мест'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        //backgroundColor: Colors.green,
+        child: ListView(
+          children: const [
+            DrawerHeader(
+              child: Center(
+                child: Text('Заголовок'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_mall),
+              title: Text('Магазины'),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_restaurant),
+              title: Text('Рестораны'),
+            ),
+            ListTile(
+              leading: Icon(Icons.theater_comedy),
+              title: Text('Театры'),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_movies),
+              title: Text('Кино'),
+            ),
+            ListTile(
+              leading: Icon(Icons.park),
+              title: Text('Парки'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Hello!'),
+            TextField(),
+          ],
+        ),
+      ),
     );
   }
 }
