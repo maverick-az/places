6dfe15e01c2fb5894c66d75118bee3aab87468d0
Andrei <zariciuc.andrei@gmail.com>
2022-04-23 12:31:57 +0300

    Colors

--- lib/constants/app_strings.dart
+++
@@ -1,9 +0,0 @@
-/// Строковые константы приложения
-class AppSrings {
-  static const String appTitle = 'Места';
-
-  /// Заголовки экранов
-  static const String scrTitleSightListScreen = 'Список интересных мест';
-
-  AppSrings._();
-}
