f14990afc15fc5761b2768c391da708213057898
Andrei <zariciuc.andrei@gmail.com>
2022-06-28 20:54:57 +0300

    Changed noImageUrl

--- lib/presets/assets/images.dart
+++ lib/presets/assets/images.dart
@@ -2,7 +2,7 @@
 class AppImages {
   // внешний источник
   static const String noImageUrl =
-      'https://media.istockphoto.com/vectors/photo-coming-soon-image-icon-vector-illustration-isolated-on-white-vector-id1193046540?k=20&m=1193046540&s=612x612&w=0&h=HQfBJLo1S0CJEsD4uk7m3EkR99gkICDdf0I52uAlk-8=';
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png';
 
   AppImages._();
 }
